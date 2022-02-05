c5dccf7c158b60b72b7a0a17e5afab41ccc33a9f
Mariana Madeira <mrn.m.92@gmail.com>
2022-02-05 15:14:14 -0300

    Agregado el mock de tareas y la interface de las mismas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task";
+
+export const TASKS:Task[] = [
+  {
+    id: 1,
+    text: "Terminar primer módulo de Angular",
+    day: 'Febrero 15 a las 12:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Hacer las compras para la cena",
+    day: 'Febrero 10 a las 12:00',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Investigar sobre Bootstrap",
+    day: 'Febrero 5 a las 12:00',
+    reminder: false,
+  },
+  {
+    id: 4,
+    text: "Leer mi Clean Code",
+    day: 'Febrero 5 a las 12:00',
+    reminder: false,
+  },
+];
